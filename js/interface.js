324ef03dc61490d42f7d22e520bfe7499f8e9cdd
rootools <baboonweb@gmail.com>
2013-03-17 03:52:33 +0400

    true change color

--- js/interface.js
+++ js/interface.js
@@ -36,3 +36,44 @@ function DrawColorPicker() {
 function DropColorPicker() {
   $('#colorPicker').remove();
 }
+
+function DrawSettingsWindow() {
+  var html = '<div id="fieldColorChanger"><center><div id="colorChanger">' +
+      '<div id="colorPickerTitle">Change your color</div>' +
+      '<div class="color_from_picker color_from_changer" id="color_picker_color_1"></div>' +
+      '<div class="color_from_picker color_from_changer" id="color_picker_color_2"></div>' +
+      '<div class="color_from_picker color_from_changer" id="color_picker_color_3"></div>' +
+      '<div class="color_from_picker color_from_changer" id="color_picker_color_4"></div>' +
+      '<div class="color_from_picker color_from_changer" id="color_picker_color_5"></div>' +
+      '<div class="color_from_picker color_from_changer" id="color_picker_color_6"></div>' +
+      '<div class="color_from_picker color_from_changer" id="color_picker_color_7"></div>' +
+      '<div class="color_from_picker color_from_changer" id="color_picker_color_8"></div>' +
+      '<div class="color_from_picker color_from_changer" id="color_picker_color_9"></div>' +
+      '<div class="color_from_picker color_from_changer" id="color_picker_color_10"></div>' +
+      '<div class="color_from_picker color_from_changer" id="color_picker_color_11"></div>' +
+      '<div class="color_from_picker color_from_changer" id="color_picker_color_12"></div>' +
+    '</div></center><div id="hideColorChanger"></div></div>';
+  $('#content').append(html);
+
+  $('#hideColorChanger').bind('click', function() {
+    $('#showColorChanger').css('display', 'block');
+    $('#fieldColorChanger').remove();
+  });
+
+  $('.color_from_picker').bind('click', function() {
+    var color = RGBtoHEX($(this).css('background-color'));
+    Users.update({color: color});
+  });
+}
+
+$(function(){
+  setTimeout(function(){
+  //DrawSettingsWindow();
+},1000);
+
+  $('#showColorChanger').bind('click', function() {
+    DrawSettingsWindow();
+    $('#showColorChanger').css('display', 'none');
+  });
+
+});
